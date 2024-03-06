2e14ea16d73f9cbd93ebec41c7d0def161f02eb8
ChillerDragon <ChillerDragon@gmail.com>
2024-03-06 11:17:57 +0800

    Activate git blame

--- init.lua
+++ init.lua
@@ -242,6 +242,14 @@ require('lazy').setup {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 300,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+      },
       signs = {
         add = { text = '+' },
         change = { text = '~' },
