41cc3723823b2c683fcea087561c0fc08f29376f
ChillerDragon <ChillerDragon@gmail.com>
2024-03-08 09:54:38 +0800

    Use typescript to fix lua config for haskell bash linter

--- lua/custom/chiller/lspconfig_shellcheck.lua
+++ lua/custom/chiller/lspconfig_shellcheck.lua
@@ -8,5 +8,17 @@
 --
 -- Not sure if that was needed. If that is the case there has to be a way to automate it
 -- this neovim config should auto install all its dependencies ideally
+--
+-- Ok trololol I was too lazy to figure out the config to show the shellcheck ids
+-- So I edited the code -.-
+-- In the auto setup I should include the following
+--
+--  git clone https://github.com/ChillerDragon/bash-language-server
+--  cd bash-language-server
+--  npm i -g pnpm
+--  pnpm install
+--  npm run reinstall-server
+--
+
 require'lspconfig'.bashls.setup{}
 
