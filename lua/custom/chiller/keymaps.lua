5cfa402310cdac1976c755e5ca9bb7c949410ba3
ChillerDragon <ChillerDragon@gmail.com>
2024-03-09 10:30:55 +0800

    Better newlines for all run commands

--- lua/custom/chiller/keymaps.lua
+++ lua/custom/chiller/keymaps.lua
@@ -12,23 +12,24 @@ vim.keymap.set('n', 'c', function()
   --       or do anything unexpected
   local run_cmd = nil
   if vim.bo.filetype == 'lua' then
-    run_cmd = '!lua %'
+    run_cmd = 'lua %'
   elseif vim.bo.filetype == 'ruby' then
-    run_cmd = '!ruby %'
+    run_cmd = 'ruby %'
   elseif vim.bo.filetype == 'javascript' then
-    run_cmd = '!node %'
+    run_cmd = 'node %'
   elseif vim.bo.filetype == 'python' then
-    run_cmd = '!python %'
+    run_cmd = 'python %'
   elseif vim.bo.filetype == 'sh' then
-    run_cmd = '!bash %'
+    run_cmd = 'bash %'
   elseif vim.bo.filetype == 'c' then
-    run_cmd = '!gcc -ggdb % -o %:r && printf "\\n" && ./%:r'
+    run_cmd = 'gcc -ggdb % -o %:r && ./%:r'
   elseif vim.bo.filetype == 'cpp' then
-    run_cmd = '!g++ -ggdb % -o %:r && printf "\\n" && ./%:r'
+    run_cmd = 'g++ -ggdb % -o %:r && ./%:r'
   elseif vim.bo.filetype == 'haskell' then
-    run_cmd = '!ghc % && printf "\\n" && ./%:r'
+    run_cmd = 'ghc % && ./%:r'
   end
   if run_cmd ~= nil then
+    run_cmd = '!printf "\\n\\n" && ' .. run_cmd
     vim.cmd(run_cmd)
   end
 end, { desc = '[C]ompile and run current buffer' })
