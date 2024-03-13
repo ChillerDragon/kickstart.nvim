e621ad3ecee7f37aca87dd2332912152ef4fa9dc
ChillerDragon <ChillerDragon@gmail.com>
2024-03-13 19:35:37 +0800

    I hate where this is going
    teeworlds specific grepping

--- init.lua
+++ init.lua
@@ -84,6 +84,9 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+-- chiller
+require 'custom/chiller/helpers'
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -400,8 +403,20 @@ require('lazy').setup {
         })
       end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sw', function ()
+        local options = {}
+        if is_teeworlds_like_source() then
+          options.search_dirs = teeworlds_src_dirs()
+        end
+        builtin.grep_string(options)
+      end, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', function ()
+        local options = {}
+        if is_teeworlds_like_source() then
+          options.search_dirs = teeworlds_src_dirs()
+        end
+        builtin.live_grep(options)
+      end, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
---
+++ lua/custom/chiller/helpers.lua
@@ -0,0 +1,26 @@
+function is_teeworlds_like_source()
+  local filename_full_path = vim.fn.expand('%:p')
+  -- TODO: detect based on file structure
+  -- CMakeListst.txt bam.lua other/ src/ data/ datasrc/ .github etc
+  if string.match(filename_full_path, "Desktop/git/ddnet/") then
+    return true
+  elseif string.match(filename_full_path, "Desktop/git/teeworlds/") then
+    return true
+  elseif string.match(filename_full_path, "Desktop/git/ddnet-insta/") then
+    return true
+  elseif string.match(filename_full_path, "Desktop/git/chillerbot-ux/") then
+    return true
+  end
+  return false
+end
+
+function teeworlds_src_dirs()
+  return {
+    "src",
+    "datasrc",
+    ".gitignore",
+    "Dockerfile",
+    "scripts",
+  }
+end
+
