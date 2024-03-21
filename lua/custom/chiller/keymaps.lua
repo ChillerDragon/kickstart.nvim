ff6fa9da5e53ffb4cc6c446fa433f46e4250f49c
ChillerDragon <ChillerDragon@gmail.com>
2024-03-21 08:43:49 +0800

    neovim is such a timesink this took me 1 min in vimscript

--- lua/custom/chiller/keymaps.lua
+++ lua/custom/chiller/keymaps.lua
@@ -28,9 +28,38 @@ vim.keymap.set('n', 'c', function()
   --       that seems more robust and should probably also be done here
   --       if the current file path is funny it should not break
   --       or do anything unexpected
+
+  local file_exists = function (name)
+    local f = io.open(name, "r")
+    return f ~= nil and io.close(f)
+  end
+
+  local cmd_if_make = function (current_cmd)
+    local makefile_path = vim.fn.getcwd() .. "/Makefile"
+    if file_exists(makefile_path) then
+      return 'make'
+    end
+    return current_cmd
+  end
+
+  local cmd_if_tw_codebase = function (current_cmd)
+    local filename_full_path = vim.fn.expand('%:p')
+
+    local run_cmd = current_cmd
+
+    if string.match(filename_full_path, "Desktop/git/ddnet/src") then
+      run_cmd = "cd ~/Desktop/git/ddnet/build/ && make -j$(nproc) "
+      -- run_cmd = run_cmd .. " && ./DDNet-Server"
+    elseif string.match(filename_full_path, "Desktop/git/teeworlds/src") then
+      run_cmd = "cd ~/Desktop/git/teeworlds/build/ && make -j$(nproc) "
+      -- run_cmd = run_cmd .. " && ./teeworlds_srv"
+    end
+
+    return run_cmd
+  end
+
   local run_cmd = nil
   local filename = vim.fn.expand('%')
-  local filename_full_path = vim.fn.expand('%:p')
   if vim.bo.filetype == 'lua' then
     run_cmd = 'lua %'
   elseif vim.bo.filetype == 'ruby' then
@@ -48,15 +77,11 @@ vim.keymap.set('n', 'c', function()
     run_cmd = 'bash %'
   elseif vim.bo.filetype == 'c' then
     run_cmd = 'gcc -ggdb % -o %:r && ./%:r'
+    run_cmd = cmd_if_make(run_cmd)
   elseif vim.bo.filetype == 'cpp' then
     run_cmd = 'g++ -ggdb % -o %:r && ./%:r'
-    if string.match(filename_full_path, "Desktop/git/ddnet/src") then
-      run_cmd = "cd ~/Desktop/git/ddnet/build/ && make -j$(nproc) "
-      -- run_cmd = run_cmd .. " && ./DDNet-Server"
-    elseif string.match(filename_full_path, "Desktop/git/teeworlds/src") then
-      run_cmd = "cd ~/Desktop/git/teeworlds/build/ && make -j$(nproc) "
-      -- run_cmd = run_cmd .. " && ./teeworlds_srv"
-    end
+    run_cmd = cmd_if_make(run_cmd)
+    run_cmd = cmd_if_tw_codebase(run_cmd)
   elseif vim.bo.filetype == 'haskell' then
     run_cmd = 'ghc % && ./%:r'
   end
