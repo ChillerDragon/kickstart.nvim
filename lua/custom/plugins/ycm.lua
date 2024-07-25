902edc71fee15ba4b09332360b6927ea84bb39f7
ChillerDragon <ChillerDragon@gmail.com>
2024-07-25 19:51:52 +0800

    homemade ycm auto install

--- lua/custom/plugins/ycm.lua
+++ lua/custom/plugins/ycm.lua
@@ -1,3 +1,14 @@
+local function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
+
 return {
-  'ycm-core/YouCompleteMe'
+  'ycm-core/YouCompleteMe',
+  config = function()
+    if file_exists(os.getenv("HOME") .. "/.local/share/nvim/lazy/YouCompleteMe/third_party/ycmd/ycm_core.cpython-311-x86_64-linux-gnu.so") then
+      return
+    end
+    vim.cmd("!python3 ~/.local/share/nvim/lazy/YouCompleteMe/install.py")
+  end
 }
