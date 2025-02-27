9db4e8677a57e41fb312fb695fdaece456c0fb7d
ChillerDragon <chillerdragon@gmail.com>
2025-02-27 18:52:39 +0100

    more flexible hack to fix debian

--- lua/custom/plugins/ycm.lua
+++ lua/custom/plugins/ycm.lua
@@ -15,9 +15,11 @@ return {
   'ycm-core/YouCompleteMe',
   config = function()
     -- omg i had to bump the 313 number
-    local ycmd_so = os.getenv("HOME") .. "/.local/share/nvim/lazy/YouCompleteMe/third_party/ycmd/ycm_core.cpython-313-x86_64-linux-gnu.so"
-    if file_exists(ycmd_so) then
-      return
+    for _, so_version in ipairs({"311", "313"}) do
+      local ycmd_so = os.getenv("HOME") .. "/.local/share/nvim/lazy/YouCompleteMe/third_party/ycmd/ycm_core.cpython-" .. so_version .. "-x86_64-linux-gnu.so"
+      if file_exists(ycmd_so) then
+        return
+      end
     end
     vim.cmd("!python3 ~/.local/share/nvim/lazy/YouCompleteMe/install.py")
   end
