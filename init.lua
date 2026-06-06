ef87ff4cb009656f1fcc61e1778e7ee6b96c60a9
ChillerDragon <chillerdragon@gmail.com>
2026-06-06 09:02:31 +0200

    emmet

--- init.lua
+++ init.lua
@@ -694,6 +694,7 @@ do
     -- gopls = {},
     pyright = {},
     superhtml = {},
+    emmet_ls = {},
     -- rust_analyzer = {},
     --
     -- Some languages (like typescript) have entire language plugins that can be useful:
