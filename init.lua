59dba8553c76e4c3b88bfc52d4d727b330eb0a8f
ChillerDragon <chillerdragon@gmail.com>
2026-06-12 13:04:24 +0200

    Use typescript lsp for my vanilla js with jsdoc comments
    Works nice for this one
    https://github.com/ChillerDragon/teeworlds-web-traffic-decoder/blob/9129a626ccebba07d086e13e700f67374f40e3b6/static/js/main.js#L9-L14

--- init.lua
+++ init.lua
@@ -703,7 +703,13 @@ do
     --    https://github.com/pmizio/typescript-tools.nvim
     --
     -- But for many setups, the LSP (`ts_ls`) will work just fine
-    -- ts_ls = {},
+    ts_ls = {
+      settings = {
+        implicitProjectConfiguration = {
+          checkJs = true
+        },
+      }
+    },
 
     stylua = {}, -- Used to format Lua code
 
