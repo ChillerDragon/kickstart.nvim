adf6e5c5bb5ce7a470001727db099ed19d064331
ChillerDragon <ChillerDragon@gmail.com>
2026-06-21 11:20:41 +0200

    rost

--- init.lua
+++ init.lua
@@ -697,7 +697,7 @@ do
     pyright = {},
     superhtml = {},
     emmet_ls = {},
-    -- rust_analyzer = {},
+    rust_analyzer = {},
     --
     -- Some languages (like typescript) have entire language plugins that can be useful:
     --    https://github.com/pmizio/typescript-tools.nvim
